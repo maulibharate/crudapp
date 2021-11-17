4b63753ae15c0249f89f7f16dd0f69f30b5359bf
maulibharate <maulibharate96@gmail.com>
2021-11-17 08:40:17 +0530

    crud app first commit

---
+++ server/database/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        mongoose.connect(process.env.DATABASE, {
+            useNewUrlParser: true,
+            // useCreateIndex: true,
+            // useUnifiedTopology: true,
+            // useFindAndModify: false
+        })
+            console.log(`DB connection successful!!`);
+        } catch(error) {
+            console.log(`no connection ${error}`)
+        };
+    }
+
+module.exports = connectDB;
